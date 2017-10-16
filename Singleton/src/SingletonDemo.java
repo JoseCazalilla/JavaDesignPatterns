a1b9e8d562cc0790d9fb44607c462ffeab152c08
Jose Cazalilla <jocamo5upv@gmail.com>
2017-10-16 11:50:48 +0200

    Singleton example

---
+++ Singleton/src/SingletonDemo.java
@@ -0,0 +1,17 @@
+public class SingletonDemo {
+    public static void main(String[] args) {
+
+        //illegal construct
+        //Compile Time Error: The constructor SingleObject() is not visible
+        //SingleObject object = new SingleObject();
+
+        //Get the only object available
+        SingleObject object = SingleObject.getInstance();
+        SingleObject object2 = SingleObject.getInstance();
+
+        //show the message
+        object.showMessage();
+        object2.showMessage();
+
+    }
+}
